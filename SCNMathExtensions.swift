26c42ab047dec994bb748f6da39d471932710636
Slipp Douglas Thompson <git@capnslipp.com>
2016-07-25 16:07:36 -0500

    New `SCNQuaternion` extensions
    * New extensions for `SCNQuaternion`:  
    	⁃ `SCNQuaternion#toGLK()` & `GLKQuaternion#toSCN()` helper methods for quaternion conversion (for the sake of GLK-powered maths).  
    	⁃ `SCNQuaternion#init(q:(Float,Float,Float,Float))` initializer & `q:(Float,Float,Float,Float)` property for better interoperability with `GLKQuaternion` _(which has both the `q:` init & `q` property)_.  
    	⁃ `SCNVector3#toGLK()` & `GLKVector3#toSCN()` helper methods for vector conversion (for the sake of GLK-powered maths).  
    	⁃ `init(from:to:)`/`init(from:to:opposing180Axis:)` initializes a quaternion to the rotation between a pair of vector directions.  
    	    ‣ Currently has a couple of different calculation methods; deciding which works best.  
    	⁃ `angleAxis()` converts a quaternion back into a `Float` rotation angle around a `SCNVector3` axis.  Uses the uber-handy `GLKQuaternionAngle()` & `GLKQuaternionAxis()` utils.  
    	⁃ `delta()` (probably not the best name) calculates the rotation to get from one rotation to another.  Uses some handy maths I learnt whilst reading https://en.wikipedia.org/wiki/Quaternion .

--- SCNMathExtensions.swift
+++ SCNMathExtensions.swift
@@ -14,10 +14,36 @@ extension SCNVector3 {
 	public func toSimd() -> float3 {
 		return SCNVector3ToFloat3(self)
 	}
+	public func toGLK() -> GLKVector3 {
+		return SCNVector3ToGLKVector3(self)
+	}
 }
 extension float3 {
 	public func toSCN() -> SCNVector3 {
 		return SCNVector3FromFloat3(self)
+	}
+}
+extension GLKVector3 {
+	public func toSCN() -> SCNVector3 {
+		return SCNVector3FromGLKVector3(self)
+	}
+}
+
+extension SCNQuaternion {
+	public var q:(Float,Float,Float,Float) {
+		return (self.x, self.y, self.z, self.w)
+	}
+	public init(q:(Float,Float,Float,Float)) {
+		self.init(x: q.0, y: q.1, z: q.2, w: q.3)
+	}
+	
+	public func toGLK() -> GLKQuaternion {
+		return GLKQuaternion(q: self.q)
+	}
+}
+extension GLKQuaternion {
+	public func toSCN() -> SCNQuaternion {
+		return SCNQuaternion(q: self.q)
 	}
 }
 
@@ -146,5 +172,50 @@ extension SCNVector3 : Equatable
 {
 	public static func == (a:SCNVector3, b:SCNVector3) -> Bool {
 		return SCNVector3EqualToVector3(a, b)
+	}
+}
+
+
+
+// MARK: SCNQuaternion Extensions
+
+extension SCNQuaternion
+{
+	init(from a:SCNVector3, to b:SCNVector3, opposing180Axis:SCNVector3=SCNVector3(0, 1, 0)) {
+		let aNormal = a.normalized(), bNormal = b.normalized()
+		let dotProduct = aNormal.dot(by: bNormal)
+		if dotProduct >= 1.0 {
+			self = GLKQuaternionIdentity.toSCN()
+		}
+		else if dotProduct < (-1.0 + Float.leastNormalMagnitude) {
+			self = GLKQuaternionMakeWithAngleAndVector3Axis(Float.pi, opposing180Axis.toGLK()).toSCN()
+		}
+		else {
+			if true {
+				// version 1, from: http://stackoverflow.com/questions/1171849/finding-quaternion-representing-the-rotation-from-one-vector-to-another
+				let xyz = aNormal.crossed(by: bNormal)
+				(self.x, self.y, self.z) = (xyz.x, xyz.y, xyz.z)
+				self.w = sqrt(aNormal.magnitudeSquared() * bNormal.magnitudeSquared()) + dotProduct
+			} else {
+				// version 2, from: https://bitbucket.org/sinbad/ogre/src/9db75e3ba05c/OgreMain/include/OgreVector3.h?fileviewer=file-view-default#OgreVector3.h-651
+				let s = sqrt((1.0 + dotProduct) * 2.0)
+				let sReciprocal = 1.0 / s
+				let xyz = aNormal.crossed(by: bNormal) * sReciprocal
+				(self.x, self.y, self.z) = (xyz.x, xyz.y, xyz.z)
+				self.w = s * 0.5
+			}
+		}
+	}
+	
+	
+	public func angleAxis() -> (Float, SCNVector3) {
+		let self_glk = self.toGLK()
+		let angle = GLKQuaternionAngle(self_glk)
+		let axis = SCNVector3FromGLKVector3(GLKQuaternionAxis(self_glk))
+		return (angle, axis)
+	}
+	
+	public func delta(_ other:SCNQuaternion) -> SCNQuaternion {
+		return GLKQuaternionMultiply(GLKQuaternionInvert(self.toGLK()), other.toGLK()).toSCN()
 	}
 }
