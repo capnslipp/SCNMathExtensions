f82b1914536ca4ce82caa3694e92962e604e6817
Slipp Douglas Thompson <git@capnslipp.com>
2019-04-20 05:25:49 -0500

    Updated for SPM; removed modulemap
    * Added all the additional modern fields to `Package.swift`— `products:`, `dependencies:`, `targets:`, and `swiftLanguageVersions:`.  
    	▸ I added all the Swift versions that the code will compile under — 3, 4, 4.2, & 5 — as `.version("…")` enum entries (instead of using the built-in `.v4`, etc. entries) because the package spec doesn't otherwise allow specifying newer-than-current entries, which is necessary when shadowing this package file for older Swift versions.  Erm, put another way, using `.version()` lets me specify when I know to be true instead of being tightly-bound to SPM version & API, like a real package manager would allow.  _(SPM's mercurial API has really gotten under my skin.)_
    * Removed `module.modulemap` due to SPM issues; now letting Xcode auto-gen it.  
    	▸ SPM apparently thinks the presence of a `.modulemap` file in the root dir means I'm trying to specify a system library, not just Obj-C compatibility.  _(It really bugs me that SPM is looking at more than `Package.swift` and things it points to, and making judgements about how my file structure should be laid out.)_  
    	▸ Largely just following the example and logic of CocoaLumberjack here: https://github.com/CocoaLumberjack/CocoaLumberjack/issues/954
    * Enabled `DEFINES_MODULE`/“Defines Module” and `CLANG_ENABLE_MODULES`/“Enable Modules (C and Objective-C)”.
    * Added `SceneKit.framework` under Linked Libraries and Frameworks, in the hopes that it'll help Xcode know that anything using this framework also needs SceneKit.  
    	▸ That doesn't actually seem to work; the generated `module.modulemap` doesn't seems to gain a `link` entry, and “Link Frameworks Automatically” in client projects doesn't seem to work.  Maybe I'm halfway there and I'll figure out what else I need later on; or maybe Xcode just needs to get its shit together.

--- Package.swift
+++ Package.swift
@@ -2,5 +2,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "SCNMathExtensions"
+	name: "SCNMathExtensions",
+	products: [
+		.library(name: "SCNMathExtensions", targets: [ "SCNMathExtensions" ]),
+	],
+	dependencies: [],
+	targets: [
+		.target(name: "SCNMathExtensions", dependencies: [], path: "Sources/"),
+	],
+	swiftLanguageVersions: [
+		.version("3"),
+		.version("4"),
+		.version("4.2"),
+		.version("5"),
+	]
 )
